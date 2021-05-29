7be8d2503e5a25c7d1d479c1c2941f7d4450c05f
yangjie01 <617958070@qq.com>
2021-05-29 16:03:29 +0800

    add: first run

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" ref="hw"/>
+  <test> </test>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import test from './components/test.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    test  //局部注册
+  },
+  created() {
+    console.log(this.$refs.hw);
+  },
+  mounted() {
+    this.$refs.hw.foo = 'testData'
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
